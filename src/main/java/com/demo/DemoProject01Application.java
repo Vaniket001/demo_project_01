7d2b0022eb3fa5e80686e726ed5d5c481d2482e8
Aniket <kumaraniket900@gmail.com>
2022-05-08 07:17:25 +0530

    First Demo Commit

---
+++ src/main/java/com/demo/DemoProject01Application.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject01Application.class, args);
+		System.out.println("DemoProject01Application Has started !!");
+	}
+
+}
---
+++ src/main/java/com/demo/rest/controller/WelcomeMsg.java
@@ -0,0 +1,14 @@
+package com.demo.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeMsg {
+	
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return "Welcome To My Demo Project";
+	}
+
+}
